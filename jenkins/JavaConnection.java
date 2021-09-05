dd51c60779a4d4e7616115d44d193084072b0a0f
Johann Pando <johann_pando@hotmail.com>
2021-09-05 14:44:56 +0100

    liqubase changes

---
+++ jenkins/JavaConnection.java
@@ -0,0 +1,18 @@
+import java.sql.*;
+
+class JavaConnection {
+    public static void main(String args[]) {
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            Connection con = DriverManager.getConnection("jdbc:mysql://db:3306/testdb", "root", "root");
+            // here sonoo is database name, root is username and password
+            Statement stmt = con.createStatement();
+            ResultSet rs = stmt.executeQuery("select * from info");
+            while (rs.next())
+                System.out.println(rs.getInt(1) + "  " + rs.getString(2) + "  " + rs.getString(3));
+            con.close();
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+}
